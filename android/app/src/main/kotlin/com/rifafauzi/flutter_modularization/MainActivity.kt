ef58d5ebb66e2f05d45fced9d077cec80d9e88be
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2019-12-09 13:18:14 +0700

    start 🔥

---
+++ android/app/src/main/kotlin/com/rifafauzi/flutter_modularization/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rifafauzi.flutter_modularization
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
